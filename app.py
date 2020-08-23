8cbfa778f5c4f89183a312e030862665332c509d
Jared Yu <qzyu999@gmail.com>
2020-08-22 22:28:57 -0700

    Moved pickle load to app.run area

--- app.py
+++ app.py
@@ -15,47 +15,47 @@ from model import sarsa
 from model import plot_paths
 
 app = Flask(__name__, template_folder="templates")
-if __name__ == "__main__":
-    # L-track
-    vi_l_1 = pickle.load(open('vi_l_1.pkl', 'rb'))
-    vi_l_2 = pickle.load(open('vi_l_2.pkl', 'rb'))
+# if __name__ == "__main__":
+    # # L-track
+    # vi_l_1 = pickle.load(open('vi_l_1.pkl', 'rb'))
+    # vi_l_2 = pickle.load(open('vi_l_2.pkl', 'rb'))
 
-    ql_l_1 = pickle.load(open('ql_l_1.pkl', 'rb'))
-    ql_l_2 = pickle.load(open('ql_l_2.pkl', 'rb'))
+    # ql_l_1 = pickle.load(open('ql_l_1.pkl', 'rb'))
+    # ql_l_2 = pickle.load(open('ql_l_2.pkl', 'rb'))
 
-    sa_l_1 = pickle.load(open('sa_l_1.pkl', 'rb'))
-    sa_l_2 = pickle.load(open('sa_l_2.pkl', 'rb'))
+    # sa_l_1 = pickle.load(open('sa_l_1.pkl', 'rb'))
+    # sa_l_2 = pickle.load(open('sa_l_2.pkl', 'rb'))
 
-    # O-track
-    vi_o_1 = pickle.load(open('vi_o_1.pkl', 'rb'))
-    vi_o_2 = pickle.load(open('vi_o_2.pkl', 'rb'))
+    # # O-track
+    # vi_o_1 = pickle.load(open('vi_o_1.pkl', 'rb'))
+    # vi_o_2 = pickle.load(open('vi_o_2.pkl', 'rb'))
 
-    ql_o_1 = pickle.load(open('ql_o_1.pkl', 'rb'))
-    ql_o_2 = pickle.load(open('ql_o_2.pkl', 'rb'))
+    # ql_o_1 = pickle.load(open('ql_o_1.pkl', 'rb'))
+    # ql_o_2 = pickle.load(open('ql_o_2.pkl', 'rb'))
 
-    sa_o_1 = pickle.load(open('sa_o_1.pkl', 'rb'))
-    sa_o_2 = pickle.load(open('sa_o_2.pkl', 'rb'))
+    # sa_o_1 = pickle.load(open('sa_o_1.pkl', 'rb'))
+    # sa_o_2 = pickle.load(open('sa_o_2.pkl', 'rb'))
 
-    # R-track
-    vi_r_1 = pickle.load(open('vi_r_1.pkl', 'rb'))
-    vi_r_2 = pickle.load(open('vi_r_2.pkl', 'rb'))
+    # # R-track
+    # vi_r_1 = pickle.load(open('vi_r_1.pkl', 'rb'))
+    # vi_r_2 = pickle.load(open('vi_r_2.pkl', 'rb'))
 
-    ql_r_1 = pickle.load(open('ql_r_1.pkl', 'rb'))
-    ql_r_2 = pickle.load(open('ql_r_2.pkl', 'rb'))
+    # ql_r_1 = pickle.load(open('ql_r_1.pkl', 'rb'))
+    # ql_r_2 = pickle.load(open('ql_r_2.pkl', 'rb'))
 
-    sa_r_1 = pickle.load(open('sa_r_1.pkl', 'rb'))
-    sa_r_2 = pickle.load(open('sa_r_2.pkl', 'rb'))
+    # sa_r_1 = pickle.load(open('sa_r_1.pkl', 'rb'))
+    # sa_r_2 = pickle.load(open('sa_r_2.pkl', 'rb'))
 
-    # Categorize the agents into lists
-    # Crash type -> Track type -> Car type
-    # [crash1_list, crash2_list]
-    # crash1_list:[l_track_list, o_track_list, r_track_list]
-    agent_list = [[[vi_l_1, ql_l_1, sa_l_1],
-                    [vi_o_1, ql_o_1, sa_o_1],
-                    [vi_r_1, ql_r_1, sa_r_1]],
-                [[vi_l_2, ql_l_2, sa_l_2],
-                    [vi_o_2, ql_o_2, sa_o_2],
-                    [vi_r_2, ql_r_2, sa_r_2]]]
+    # # Categorize the agents into lists
+    # # Crash type -> Track type -> Car type
+    # # [crash1_list, crash2_list]
+    # # crash1_list:[l_track_list, o_track_list, r_track_list]
+    # agent_list = [[[vi_l_1, ql_l_1, sa_l_1],
+    #                 [vi_o_1, ql_o_1, sa_o_1],
+    #                 [vi_r_1, ql_r_1, sa_r_1]],
+    #             [[vi_l_2, ql_l_2, sa_l_2],
+    #                 [vi_o_2, ql_o_2, sa_o_2],
+    #                 [vi_r_2, ql_r_2, sa_r_2]]]
 
 # Declare your table
 class ItemTable(Table):
@@ -140,4 +140,45 @@ def race():
 
 
 if __name__ == "__main__":
+    # L-track
+    vi_l_1 = pickle.load(open('vi_l_1.pkl', 'rb'))
+    vi_l_2 = pickle.load(open('vi_l_2.pkl', 'rb'))
+
+    ql_l_1 = pickle.load(open('ql_l_1.pkl', 'rb'))
+    ql_l_2 = pickle.load(open('ql_l_2.pkl', 'rb'))
+
+    sa_l_1 = pickle.load(open('sa_l_1.pkl', 'rb'))
+    sa_l_2 = pickle.load(open('sa_l_2.pkl', 'rb'))
+
+    # O-track
+    vi_o_1 = pickle.load(open('vi_o_1.pkl', 'rb'))
+    vi_o_2 = pickle.load(open('vi_o_2.pkl', 'rb'))
+
+    ql_o_1 = pickle.load(open('ql_o_1.pkl', 'rb'))
+    ql_o_2 = pickle.load(open('ql_o_2.pkl', 'rb'))
+
+    sa_o_1 = pickle.load(open('sa_o_1.pkl', 'rb'))
+    sa_o_2 = pickle.load(open('sa_o_2.pkl', 'rb'))
+
+    # R-track
+    vi_r_1 = pickle.load(open('vi_r_1.pkl', 'rb'))
+    vi_r_2 = pickle.load(open('vi_r_2.pkl', 'rb'))
+
+    ql_r_1 = pickle.load(open('ql_r_1.pkl', 'rb'))
+    ql_r_2 = pickle.load(open('ql_r_2.pkl', 'rb'))
+
+    sa_r_1 = pickle.load(open('sa_r_1.pkl', 'rb'))
+    sa_r_2 = pickle.load(open('sa_r_2.pkl', 'rb'))
+
+    # Categorize the agents into lists
+    # Crash type -> Track type -> Car type
+    # [crash1_list, crash2_list]
+    # crash1_list:[l_track_list, o_track_list, r_track_list]
+    agent_list = [[[vi_l_1, ql_l_1, sa_l_1],
+                    [vi_o_1, ql_o_1, sa_o_1],
+                    [vi_r_1, ql_r_1, sa_r_1]],
+                [[vi_l_2, ql_l_2, sa_l_2],
+                    [vi_o_2, ql_o_2, sa_o_2],
+                    [vi_r_2, ql_r_2, sa_r_2]]]
+
     app.run(debug=True)
